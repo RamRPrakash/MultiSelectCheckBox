e8d851af896c4c585fc177c93ccc2877a1db952d
RamRPrakash <prakashparthi1996@gmail.com>
2020-01-21 16:09:26 +0530

    Changes multi

--- src/MultiSelectCheck.js
+++ src/MultiSelectCheck.js
@@ -6,37 +6,15 @@ import MenuItem from "@material-ui/core/MenuItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Select from "@material-ui/core/Select";
 import Checkbox from "@material-ui/core/Checkbox";
-
-const useStyles = makeStyles(theme => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-    maxWidth: 300
-  },
-  chips: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  chip: {
-    margin: 2
-  },
-  noLabel: {
-    marginTop: theme.spacing(3)
-  }
-}));
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
 const MenuProps = {
   PaperProps: {
     style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      // maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
       width: 250
     }
   }
 };
-
-const names = [
+var names = [
   "Oliver Hansen",
   "Van Henry",
   "April Tucker",
@@ -57,12 +35,13 @@ class MultiSelectCheck extends Component {
     };
   }
   handleChange = e => {
+    var data = e.target.value;
     this.setState({
-      personName: this.state.personName.push(e.target.value)
+      personName: data
     });
+    console.log(this.state.personName);
   };
   render() {
-    let personName = this.state.personName;
     return (
       <div>
         <div>
